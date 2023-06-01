fcfa366f000f7e2db655ecb698edc23e8216c308
Samiullah <samiullahk997@gmail.com>
2023-06-02 01:05:13 +0500

    Update app.js file to implement sign-up functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "./Routes.js";
+import { Login, Signup } from "./Routes.js";
 
 const App = () => {
   return (
@@ -9,6 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </Fragment>
