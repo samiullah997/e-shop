b3a73d50a5de4b8b2b77a0175645b405379b1bc8
Samiullah <samiullahk997@gmail.com>
2023-06-01 13:57:16 +0500

    setup complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import './App.css'
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./Routes.js";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  );
+};
 
-export default App
+export default App;
